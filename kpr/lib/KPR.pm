825f9fdc65c66911090621d987b3688fdd90cdee
Ryota Wada <x2w@x2w.org>
2012-02-18 03:45:03 +0900

    renamed:    kpr/make-instances.pl -> kpr/_make-instances.pl 	modified:   kpr/lib/KPR.pm 	new file:   kpr/session/empty 	modified:   kpr/skeleton/doc_create_form.html 	modified:   kpr/skeleton/login_form.html 	modified:   kpr/skeleton/menu_form.html

--- kpr/lib/KPR.pm
+++ kpr/lib/KPR.pm
@@ -2,7 +2,7 @@
 # KPR.pm
 #
 #   Author: Ryota Wada
-#     Date: Sat Feb 18 00:35:13 2012. (JST)
+#     Date: Sat Feb 18 03:42:23 2012. (JST)
 #
 package KPR;
 use strict;
@@ -10,33 +10,137 @@ use warnings;
 use base 'CGI::Application';
 
 use CGI::Application::Plugin::ConfigAuto (qw/cfg/);
+use CGI::Application::Plugin::Session;
+use CGI::Application::Plugin::Redirect;
 
-sub setup {
-    my $c = shift;
-
-    $c->run_modes(
-        'login_form' => \&x_form,
-        'menu_form' => \&x_form,
-        'doc_create_form', => \&x_form,
-        'doc_delete_form', => \&x_form,
-        'doc_update_form', => \&x_form,
-        'dir_create_form', => \&x_form,
-        'dir_delete_form', => \&x_form,
-        'dir_update_form', => \&x_form,
+sub cgiapp_init {
+    my $self = shift;
+    $self->SUPER::cgiapp_init(@_);
+    $self->session_config(
+        CGI_SESSION_OPTIONS => [ "driver:File", $self->query, { Directory => './session' }, ],
+        COOKIE_PARAMS       => { -path   => '/',
+                                -expires => '+2h', },
+        SEND_COOKIE         => 1,
     );
-    $c->mode_param('resource');
-    $c->query->param('resource', $c->param('resource') );
+}
+sub setup {
+    my $self = shift;
 
-    $c->tmpl_path($c->cfg('TemplateDirectory'));
+    $self->run_modes(
+        'login_form' => \&kpr_login,
+        'menu_form' => \&kpr_menu,
+        'doc_create_form' => \&kpr_doc_create,
+        'doc_delete_form' => \&x_form,
+        'doc_update_form' => \&x_form,
+        'dir_create_form' => \&x_form,
+        'dir_delete_form' => \&x_form,
+        'dir_update_form' => \&x_form,
+    );
+    $self->mode_param('resource');
+    $self->query->param('resource', $self->param('resource') );# overriding 
+    $self->tmpl_path($self->cfg('TemplateDirectory'));
 
+    $self->param('WebsiteDirectory', $self->cfg('WebsiteDirectory'));
     # debug
     # open my $fh, '>', 'test.txt' or die $!;
-    # my %d = $c->cfg;
-    # print $fh $c->tmpl_path();
+    # my %d = $self->cfg;
+    # print $fh $self->tmpl_path();
     # close $fh;
 
 }
 
+sub kpr_login {
+    my $self = shift;
+    my $errs = shift;
+    
+    my $q = $self->query;
+
+    if ($q->param('MODE') eq 'logout') {
+        if (defined $q->param('PASSWORD') and $q->param('PASSWORD') eq 'qwerty') {
+            $self->session->delete;
+        }
+        else {
+            # certification error routine
+        }
+    }
+    elsif ($q->param('MODE') eq 'login') {
+        if (defined $q->param('PASSWORD') and $q->param('PASSWORD') eq 'qwerty') {
+            $self->session->new;
+            $self->redirect('menu_form.cgi');
+        }
+        else {
+            # certification error routine
+        }
+    }
+    else { # default
+        my $t = $self->load_tmpl;
+        $t->param($errs) if $errs;
+        return $t->output;
+    }
+}
+sub kpr_menu {
+    my $self = shift;
+    my $errs = shift;
+
+    my $q = $self->query;
+
+    if ($q->param('MODE') eq "command") {
+        if (defined $self->run_modes($q->param('KPRCOMMAND'))) {
+            $self->redirect($q->param('KPRCOMMAND') . '.cgi');
+        }
+        else {
+            # error routine
+        }
+    }
+    else { # default
+        my $t = $self->load_tmpl;
+        $t->param($errs) if $errs;
+        return $t->output;
+    }
+}
+sub kpr_doc_create {
+    my $self = shift;
+    my $errs = shift;
+
+    my $q = $self->query;
+
+    if ($q->param('MODE') eq "command") {
+        my $file_path = $self->cfg('WebsiteDirectory') . $self->cfg('SiteID') . $q->param('ID') . '.html';
+
+        my $fh;
+        open $fh, '>', $file_path 
+            or die $!;
+        print $fh <<__HERE__;
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="ja">
+<head>
+  <meta name="author" content="">
+  <meta name="description" content="$q->param('DESC')">
+  <meta name="keyword" content="">
+  <link rel="stylesheet" href="" type="text/css">
+  <title>$q->param('TITLE')</title>
+</head>
+<body>
+
+<h1>$q->param('TITLE')</h1>
+
+__HERE__
+        print $fh $q->param('BODY');
+        print $fh <<__HERE__;
+</body>
+</html>
+__HERE__
+        $self->redirect('menu_form.cgi');
+    }
+    elsif ($q->param('MODE') eq "confirm") {
+        # confirm routine
+    }
+    else { # default
+        my $t = $self->load_tmpl;
+        $t->param($errs) if $errs;
+        return $t->output;
+    }
+}
 sub x_form { 
     my $self = shift;
     my $errs = shift;
@@ -66,12 +170,25 @@ sub _form_profile {
 
 sub form_success { } 
 
+
 sub cgiapp_prerun {
+    my $self = shift;
+ 
+    # Redirect to login, if necessary
+    unless (
+        1
+        # $self->session->param('logged_in')
+    ) {
+        $self->redirect('login_form.cgi');
+    }
+    else {
+        $self->header_add( -type => 'text/html; charset=UTF-8' );
+    }
 }
-# http://digit.que.ne.jp/work/index.cgi?Perl%E3%83%A1%E3%83%A2%2FCGI%3A%3AApplication%E3%83%A2%E3%82%B8%E3%83%A5%E3%83%BC%E3%83%AB
+
 sub cgiapp_postrun {
     my $self = shift;
-    $self->header_add( -type => 'text/html; charset=UTF-8' );
+
 }
 
 1;
